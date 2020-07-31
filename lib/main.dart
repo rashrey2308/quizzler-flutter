b9c3d6cb7ba7b5675ec1a92be9cd6c12f12f9cc8
rashrey2308 <rashrey2308@gmail.com>
2020-07-31 22:39:07 +0530

    changes added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'quizbrain.dart';
+
+Quiz_brain quiz_brain = new Quiz_brain();
 
 void main() => runApp(Quizzler());
 
@@ -25,6 +28,25 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  List<Icon> score = [];
+
+  void check(bool s) {
+    setState(() {
+      if (quiz_brain.getans() == s)
+        score.add(Icon(
+          Icons.check,
+          color: Colors.green,
+        ));
+      else
+        score.add(Icon(
+          Icons.close,
+          color: Colors.red,
+        ));
+
+      quiz_brain.next();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -37,7 +59,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                quiz_brain.getquestext(),
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -62,6 +84,7 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked true.
+                check(true);
               },
             ),
           ),
@@ -80,11 +103,14 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked false.
+                check(false);
               },
             ),
           ),
         ),
-        //TODO: Add a Row here as your score keeper
+        Row(
+          children: score,
+        )
       ],
     );
   }
---
+++ lib/questions.dart
@@ -0,0 +1,9 @@
+class Question{
+  String questiontext;
+  bool ans;
+
+  Question(String q, bool a){
+    questiontext=q;
+    ans=a;
+  }
+}
